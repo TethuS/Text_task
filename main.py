39c02caae8e6eebcf30d9f4f8c5df207167bcf77
PolinaK <pokky-66@yandex.ru>
2021-09-16 16:07:12 +0300

    Работа над указанными траблами: 1. Работа с файлами через with 2. Поправлено форматирование 3. Построчное чтение Отдельно разбила на ф-ии

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_deletingt_lines(path, num_str):
+
+    with open(path, "r") as file:
+        str_count = get_count_of_lines(path)
+        del_list = get_rand_lines_for_del(num_str, str_count)
+        with open(path[:-4] + "_res.txt", "w") as new_file:
+            for str_num, line in enumerate(file):
+                if str_num + 1 not in del_list:
+                    new_file.write(line)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# подсчитываем строки в файле
+def get_count_of_lines(path):
+    with open(path, "r") as file:
+        count = 0
+        for line in file:
+            count += 1
+    return count
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# создаем список рандомных строк, которые позже будем удалять из файла
+def get_rand_lines_for_del(num_str, str_count):
+    lines_to_del = []
+    for i in range(num_str):
+        num = random.randint(0, str_count - 1)
+        lines_to_del.append(num)
+    return lines_to_del
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    path = input("Введите путь к файлу и его имя. \nПример: C:\path\\name.txt \n >>")
+    #Не получается подставить функцию значение 14 по умолчанию
+    # выдает ошибку ValueError: invalid literal for int() with base 10: ''
+    # работает лишь через костыль в инпуте
+    num_str = int(input("Введите число удаляемых строк (по умолчанию 14): \n >>") or 14)
+    get_deletingt_lines(path, num_str)
+
+main()
